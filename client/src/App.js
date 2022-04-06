4dde8e2fea350ea68a87150391f30d714c015064
idenccev <idenchev@gmail.com>
2022-04-06 10:46:32 +0300

    Setup the Front-End and creating a routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Home from "./components/Home/Home";
+import MovieDescription from "./components/MovieDescription/MovieDescription";
+import Navigation from "./components/Navigation/Navigation";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <Navigation />
       </header>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/search" element={<Search />} />
+        <Route exact path="/:movieTitle" element={<MovieDescription />} />
+      </Routes>
     </div>
   );
 }
