4927d66e709593fb3bb4cf6e0a2015abbe9c6a9f
idenccev <idenchev@gmail.com>
2022-04-06 10:57:07 +0300

    Front-End: login and register routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 import MovieDescription from "./components/MovieDescription/MovieDescription";
 import Navigation from "./components/Navigation/Navigation";
+import Register from "./components/Register/Register";
 import Search from "./components/Search/Search";
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
       </header>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
         <Route exact path="/search" element={<Search />} />
         <Route exact path="/:movieTitle" element={<MovieDescription />} />
       </Routes>
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Login() {
+  return <div className="navigation">Login</div>;
+}
+
+export default Login;
